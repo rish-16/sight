5c5f85f3ad4afe2352e2db158eaca5607675dcb6
rish-16 <mail.rishabh.anand@gmail.com>
2019-12-04 08:16:23 +0800

    Removed write_vidsource method

--- sight.py
+++ sight.py
@@ -6,35 +6,30 @@ class Sightseer(object):
 	def __init__(self, weights_path):
 		self.weights_path = weights_path
 
-	def open_vidsource(self, write_data=False, set_gray=True, kill_key="q"):
+	def open_vidsource(self, write_data=False, set_gray=True, kill_key="q", width=160, height=120):
 
 		cap = cv2.VideoCapture(0)
-		cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
-		cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)		
+		cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+		cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-		if write_data:
-			self.write_vidsource()
+		frames = []
 
 		while True:
 			ret, frame = cap.read()
 
 			if set_gray:
 				new_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				frames.append(new_frame)
 				cv2.imshow('frame', new_frame)
 			else:
 				cv2.imshow('frame', frame)
+				frames.append(frame)
 
 			if cv2.waitKey(1) & 0xFF == ord(kill_key):
 				break
 
 		cap.release()
-		cv2.destroyAllWindows()				
-			
-	def write_vidsource(self):
-		pass
+		cv2.destroyAllWindows()	
 
-class YOLOClient(object):
-	def __init__(self):
-		pass
-	
-class 
+		if write_data:
+			return frames
---
+++ zoo.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+class YOLOClient(object):
+	def __init__(self):
+		pass
