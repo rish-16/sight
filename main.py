3f75ce7d5dabf4301407044694910c91e64b9ecd
rish-16 <mail.rishabh.anand@gmail.com>
2020-01-09 22:18:01 +0800

    Updated webcam config and added screen record

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from sight import Sightseer
-from zoo import YOLO9000Client
-
-yolonet = YOLO9000Client()
-yolonet.download_model()
 
 ss = Sightseer()
-data = ss.open_vidsource(set_gray=True, write_data=True)
+# data = ss.screen_grab(set_gray=False)
+data = ss.webcam(set_gray=False)
 
-yolonet.get_predictions(data)
+print (data)
--- sight.py
+++ sight.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 import numpy as np
+from PIL import ImageGrab
 import cv2
 
 class Sightseer(object):
-	def open_vidsource(self, write_data=False, set_gray=True, kill_key="q", width=160, height=120):
+	def webcam(self, return_data=False, set_gray=True, kill_key="q", width=160, height=120):
 
 		cap = cv2.VideoCapture(0)
 		cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -16,19 +17,53 @@ class Sightseer(object):
 
 			if set_gray:
 				new_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				new_frame = cv2.flip(new_frame, 1)
 				frames.append(new_frame)
 				cv2.imshow('frame', new_frame)
 			else:
+				frame = cv2.flip(frame, 1)
 				cv2.imshow('frame', frame)
 				frames.append(frame)
 
 			if cv2.waitKey(1) & 0xFF == ord(kill_key):
+				frames = np.array(frames)
+
+				if return_data:
+					return frames
+					
 				break
 
 		cap.release()
 		cv2.destroyAllWindows()	
+		
+	def screen_grab(self, set_gray=True, write_data=True, return_data=True, kill_key="q", filename='output.avi', width=400, height=400):
+		fourcc = cv2.VideoWriter_fourcc(*'XVID')
+		out = cv2.VideoWriter(filename, fourcc, 20.0, (640, 480))
+
+		frames = []
+
+		while True:
+			img = ImageGrab.grab(bbox=(0, 0, width, height))
+			imcv = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+
+			if write_data:
+				out.write(imcv)
+
+			if set_gray:
+				new_frame = cv2.cvtColor(np.asarray(img), cv2.COLOR_BGR2GRAY)
+				frames.append(new_frame)
+				cv2.imshow('frame', new_frame)
+			else:
+				cv2.imshow('frame', imcv)
+				frames.append(imcv)
+
+			if cv2.waitKey(1) & 0xFF == 113:
+				break
+
+		out.release()
+		cv2.destroyAllWindows()
 
 		frames = np.array(frames)
 
-		if write_data:
+		if return_data:
 			return frames
