602bc9f64ff716f406486ace32b3cc2427960f25
rish-16 <mail.rishabh.anand@gmail.com>
2020-01-11 01:22:23 +0800

    Added xml2csv method

--- proc.py
+++ proc.py
@@ -13,26 +13,26 @@ class DataAnnotator(object):
 				return i + 1
 			else:
 				return None
+		
+	def __list_to_csv(self.annotations, outfile):
+		columns = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+		xml_df = pd.DataFrame(annotations, columns=columns)
+		xml_df.to_csv(outfile, index=None)
 
-	def xml_to_csv(self, xmlpath):
-		xml_list = []
-		for xml_file in glob.glob(path + '/*.xml'):
+	def xml2csv(self, xmlpath, csvpath):
+		annotations = []
+		for xml_file in glob.glob(xml_dir + '/*.xml'):
 			tree = ET.parse(xml_file)
 			root = tree.getroot()
 			for member in root.findall('object'):
 				value = (root.find('filename').text,
-							int(root.find('size')[0].text),
-							int(root.find('size')[1].text),
-							member[0].text,
-							int(member[4][0].text),
-							int(member[4][1].text),
-							int(member[4][2].text),
-							int(member[4][3].text)
-							)
-				xml_list.append(value)
-		column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax'] # usual 
-		xml_df = pd.DataFrame(xml_list, columns=column_name)
-		return xml_df
+						 int(root.find('size')[0].text),
+						 int(root.find('size')[1].text), member[0].text,
+						 int(member[4][0].text), int(member[4][1].text),
+						 int(member[4][2].text), int(member[4][3].text))
+				annotations.append(value)
+
+		self.__list_to_csv(annotations, output_file)
 
 	def xml_to_tfrecord(self, data):
 		pass
