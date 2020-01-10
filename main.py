fb758982d109fc03a65b2e7fa792fa1fc10778e2
rish-16 <mail.rishabh.anand@gmail.com>
2020-01-11 01:30:40 +0800

    Testing xml_to_csv method in Proc

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from sight import Sightseer
+import proc
 
 ss = Sightseer()
 data = ss.load_webcam(set_gray=False, return_data=True)
+
+proc.xml_to_csv("./train/xml/", "/train/csv/")
 
 print (data.shape)
