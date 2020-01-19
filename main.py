237a64123708e9a8ac8da9a552b01ca1b232b42d
rish-16 <mail.rishabh.anand@gmail.com>
2020-01-19 23:17:05 +0800

    Almost completed NMS and decoding methods

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from sight import Sightseer
 from zoo import YOLO9000Client
 
 yolo = YOLO9000Client()
-yolo.load_model()
+yolo.load_model() # downloading and configuring weights into model
 
+ss = Sightseer()
+img = ss.load_image("./test_data/img/street.jpg")
+new_img = yolo.preprocess(img)
+
+preds = yolo.get_predictions(new_img)
+ss.render_image(new_img, preds) # renders image on pyplot
--- zoo.py
+++ zoo.py
@@ -1,107 +1,20 @@
 import os
 import wget
+import struct
 import shutil
 
 import cv2
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Input, UpSampling2D, concatenate
+from tensorflow.keras.models import Model
 
-from blocks import Layer
-
-class SightLoader():
-	def __init__(self, weights_path):
-		with open(weights_path, 'rb') as wf:
-			major, = struct.unpack('i', wf.read(4))
-			minor, = struct.unpack('i', wf.read(4))
-			revision, = struct.unpack('i', wf.read(4))
-
-            if (major*10+ minor) >= 2 and major < 1000 and minor < 1000:
-                wf.read(8)
-            else:
-                wf.read(4)
-
-            transpose = (major > 1000) or (minor > 1000)
-
-            binary = wf.read()
-
-		self.offset = 0
-		self.all_weights = np.frombuffer(binary, dtype="float32")
-
-	def read_bytes(self, chunk_size):
-		self.offset = self.offset + chunk_size
-		return self.all_weights[self.offset - chunk_size:self.offset]
-
-	def load_weights(self, model, verbose=True):
-		for i in range(106): # standard darknet layer count
-            try:
-                conv_layer = model.get_layer("conv_" + str(i))
-                if verbose:
-                    print ("Loading Convolution #{}".format(i))
-
-                if i not in [81, 93, 105]:
-                    norm_layer = model.get_layer("bnorm_" + str(i))
-
-                    size = np.prod(norm_layer.get_weights()[0].shape)
-
-                    beta = self.read_bytes(size)
-                    gamma = self.read_bytes(size)
-                    mean = self.read_bytes(size)
-                    var = self.read_bytes(size)
-
-                    weights = norm_layer.set_weights([gamma, beta, mean, var])
-
-                if len(conv_layer.get_weights()) > 1:
-                    bias = self.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
-                    kernel = self.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-
-                    kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-                    kernel = kernel.transpose([2, 3, 1, 0])
-                    conv_layer.set_weights([kernel, bias])
-                else:
-                    kernel = self.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-                    kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-                    kernel = kernel.transpose([2, 3, 1, 0])
-                    conv_layer.set_weights([kernel])
-
-            except ValueError:
-                if verbose:
-                    print ("No Convolution {}".format(i))
-                else:
-                    pass
-
-        if verbose:
-            print ("Finished loading weights into model. Predicting on input data...")
-
-	def reset_offset(self):
-		self.offset = 0
-
-class BoundingBox(object):
-	def __init__(self, xmin, ymin, xmax, ymax, objness=None, classes=None):
-        self.xmin = xmin
-        self.ymin = ymin
-        self.xmax = xmax
-        self.ymax = ymax
-
-        self.objness = objness
-        self.classes = classes
-
-        self.label = -1
-        self.confidence = -1
-
-    def get_label(self):
-        if self.label == -1:
-            self.label = np.argmax(self.classes)
-        return self.label
-
-    def get_confidence(self):
-        if self.confidence == -1:
-            self.confidence = self.classes[self.get_label()]
-        return self.confidence
+from .blocks import ConvBlock, BoundingBox, SightLoader
 
 class YOLO9000Client(object):
 	def __init__(self):
 		self.data = None	
-		self.labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+		self.all_labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
 						"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
 						"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
 						"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
@@ -111,8 +24,13 @@ class YOLO9000Client(object):
 						"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
 						"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
 						"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+		self.nms_threshold = 0.45
+		self.obj_threshold = 0.5
+		self.net_h, self.net_w = 416, 416
+		self.bbox_anchors = [[116, 90, 156, 198, 373, 326], [30, 61, 62, 45, 59, 119], [10, 13, 16, 30, 33, 23]]
+		self.yolo_model = None
 
-	def download_model(self):
+	def download_weights(self):
 		"""
 		Downloads the weights and checkpoints from 
 		online and saves them locally
@@ -134,7 +52,90 @@ class YOLO9000Client(object):
 
 			print ("\n\nWeights and config file downloaded successfully!")	
 
-	def non_maximum_suppression(bboxes, nms_thresh):
+	def load_architecture(self):
+		"""
+		Returns tf.keras.models.Model instance
+		"""
+		inp_img = Input(shape=[None, None, 3])
+
+		x = ConvBlock.get_conv_block(inp_img, [{'filter': 32, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 0},
+										{'filter': 64, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 1},
+										{'filter': 32, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 2},
+										{'filter': 64, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 3}])
+
+		x = ConvBlock.get_conv_block(x, [{'filter': 128, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 5},
+							{'filter':  64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 6},
+							{'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 7}])
+
+		x = ConvBlock.get_conv_block(x, [{'filter':  64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 9},
+							{'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 10}])
+
+		x = ConvBlock.get_conv_block(x, [{'filter': 256, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 12},
+							{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 13},
+							{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 14}])
+
+		for i in range(7):
+			x = ConvBlock.get_conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 16+i*3},
+								{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 17+i*3}])
+			
+		skip_36 = x
+			
+		x = ConvBlock.get_conv_block(x, [{'filter': 512, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 37},
+							{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 38},
+							{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 39}])
+
+		for i in range(7):
+			x = ConvBlock.get_conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 41+i*3},
+								{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 42+i*3}])
+			
+		skip_61 = x
+			
+		x = ConvBlock.get_conv_block(x, [{'filter': 1024, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 62},
+							{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 63},
+							{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 64}])
+
+		for i in range(3):
+			x = ConvBlock.get_conv_block(x, [{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 66+i*3},
+								{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 67+i*3}])
+			
+		x = ConvBlock.get_conv_block(x, [{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 75},
+							{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 76},
+							{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 77},
+							{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 78},
+							{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 79}], skip=False)
+
+		yolo_82 = ConvBlock.get_conv_block(x, [{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 80},
+									{'filter':  255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 81}], skip=False)
+
+		x = ConvBlock.get_conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 84}], skip=False)
+		x = UpSampling2D(2)(x)
+		x = concatenate([x, skip_61])
+
+		x = ConvBlock.get_conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 87},
+							{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 88},
+							{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 89},
+							{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 90},
+							{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 91}], skip=False)
+
+		yolo_94 = ConvBlock.get_conv_block(x, [{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 92},
+									{'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 93}], skip=False)
+
+		x = ConvBlock.get_conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True,   'layer_idx': 96}], skip=False)
+		x = UpSampling2D(2)(x)
+		x = concatenate([x, skip_36])
+
+		yolo_106 = ConvBlock.get_conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 99},
+									{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 100},
+									{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 101},
+									{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 102},
+									{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 103},
+									{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 104},
+									{'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 105}], skip=False)
+
+		model = Model(inp_img, [yolo_82, yolo_94, yolo_106])    
+		return model
+
+	def non_maximum_suppression(bboxes):
 		if len(bboxes) > 0:
 			nb_class = len(bboxes[0].classes)
 		else:
@@ -151,24 +152,29 @@ class YOLO9000Client(object):
 				for j in range(i+1, len(sorted_indices)):
 					index_j = sorted_indices[j]
 
-					if bbox_iou(bboxes[index_i], bboxes[index_j]) >= nms_thresh:
-						bboxes[index_j].classes[c] = 0		
+					if bbox_iou(bboxes[index_i], bboxes[index_j]) >= self.nms_threshold:
+						bboxes[index_j].classes[c] = 0
 
-class MaskRCNNClient(object):
-	def __init__(self, data):
-		self.data = data
+	def 
 
-	def load_weights(self, weights_path):
+	def load_model(self, default_path="./bin/yolov3.weights"):
 		"""
-		Search for weights and load into model
+		Downloads weights and config, loads checkpoints into architecture
 		"""
-		pass
+		self.download_weights() # downloading weights from online
+		loader = SightLoader(default_path)
+		
+		self.yolo_model = self.load_architecture() # loading weights into model
+		loader.load_weights(yolo_model)
 
-	def get_predictions(self, save_data=True, render=True):
-		"""
-		Returns array of dictionaries of all probable classes
-		with bounding boxes and confidence scores.
+	def get_predictions(self, image):
+		img_h, img_w = image.shape[:2]
 
-		Each element in the array is for each input image.
-		"""
-		pass
+		preds = self.yolo_model.predict(image)
+		boxes = []
+
+		for i in range(len(preds)):
+			boxes += self.decode_output(preds[i][0])
+
+		self.rectify_bboxes(boxes)
+		self.non_maximum_suppression(boxes)
