b9b9089267365c4ce420f24c8a4531f1386822dd
rish-16 <mail.rishabh.anand@gmail.com>
2020-01-26 00:59:55 +0800

    Updates to main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sight.sight import Sightseer
+from sight.sight import Sightseer as ss
 from sight.zoo import YOLOv3Client
 
 # downloading and configuring weights and hyperparams
@@ -9,6 +9,6 @@ yolo.load_model()
 ss = Sightseer("./test_data/img/street2.jpeg")
 image = ss.load_image()
 
-# Getting bounding boxes and displaying image
+# obtaining bounding boxes and displaying image
 preds, pred_img = yolo.predict(image, return_img=True, verbose=True)
 ss.render_image(pred_img)
